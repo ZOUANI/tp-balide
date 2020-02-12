607a1118a28e554983197b1c4128b29248e798df
ZOUANI <zouani.younes@gmail.com>
2020-02-12 12:05:24 +0100

    adding message
    waloooo

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("tp bqlideeeeeee");
+         System.out.println("tp pull implique build jenkins");
     }
     
 }
