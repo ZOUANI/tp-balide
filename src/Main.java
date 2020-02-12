5e186036bd054c1f829704b7bda3f3d20108bd31
ZOUANI <zouani.younes@gmail.com>
2020-02-12 12:39:55 +0100

    koko
    koko

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("tp bqlideeeeoneee loloooo");
+        System.out.println("tp bqlideeeeoneee koko");
    
     }
     
